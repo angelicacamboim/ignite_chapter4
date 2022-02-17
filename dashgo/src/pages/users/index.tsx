5bc74cb38c35256e88805918d49bc99776c64306
Angelica Bayer <angelicacamboim@gmail.com>
2022-02-17 20:36:30 -0300

    mirage and react query

--- dashgo/src/pages/users/index.tsx
+++ dashgo/src/pages/users/index.tsx
@@ -12,15 +12,44 @@ import {
 	Tr,
 	Td,
 	Text,
-	useBreakpointValue
+	useBreakpointValue,
+	Spinner
 } from '@chakra-ui/react'
 import Sidebar from '../../components/Sidebar'
 import Header from '../../components/Header'
 import { RiAddLine, RiPencilLine } from 'react-icons/ri'
 import Pagination from '../../components/Pagination'
 import Link from 'next/link'
+//cache de users
+import { useQuery } from 'react-query'
 
 export default function UserList() {
+	const { data, isLoading, error } = useQuery(
+		'users',
+		async () => {
+			const response = await fetch('http://localhost:3000/api/users')
+			const data = await response.json()
+
+			const users = data.users.map((user) => {
+				return {
+					id: user.id,
+					name: user.name,
+					email: user.email,
+					createAt: new Date(user.createAt).toLocaleDateString('pt-BR', {
+						day: '2-digit',
+						month: 'long',
+						year: 'numeric'
+					})
+				}
+			})
+
+			return users
+		},
+		{
+			staleTime: 1000 * 5 // 5 segundos
+		}
+	)
+
 	const isWideVersion = useBreakpointValue({
 		base: false,
 		lg: true
@@ -52,49 +81,68 @@ export default function UserList() {
 						</Link>
 					</Flex>
 
-					<Table colorScheme="whiteAlpha">
-						<Thead>
-							<Tr>
-								<Th px={['4', '4', '6']} color="gray.300" width="8">
-									<Checkbox colorScheme="pink" />
-								</Th>
+					{isLoading ? (
+						<Flex justify="center">
+							<Spinner />
+						</Flex>
+					) : error ? (
+						<Flex justify="center">
+							<Text>Falha ao obter dados dos usuários</Text>
+						</Flex>
+					) : (
+						<>
+							<Table colorScheme="whiteAlpha">
+								<Thead>
+									<Tr>
+										<Th px={['4', '4', '6']} color="gray.300" width="8">
+											<Checkbox colorScheme="pink" />
+										</Th>
 
-								<Th>Usuário</Th>
-								{isWideVersion && <Th>Data de cadastro</Th>}
-								<Th width="8"></Th>
-							</Tr>
-						</Thead>
+										<Th>Usuário</Th>
+										{isWideVersion && <Th>Data de cadastro</Th>}
+										<Th width="8"></Th>
+									</Tr>
+								</Thead>
 
-						<Tbody>
-							<Tr>
-								<Td px={['4', '4', '6']}>
-									<Checkbox colorScheme="pink" />
-								</Td>
-								<Td>
-									<Box>
-										<Text fontWeight="bold">Angelica Bayer</Text>
-										<Text fontSize="sm" color="gray.300">
-											angelicacamboim@gmail.com
-										</Text>
-									</Box>
-								</Td>
-								{isWideVersion && <Td>28 de janeiro, 2022</Td>}
-								<Td>
-									<Button
-										as="a"
-										size="sm"
-										fontSize="sm"
-										colorScheme="purple"
-										leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-									>
-										Editar
-									</Button>
-								</Td>
-							</Tr>
-						</Tbody>
-					</Table>
-
-					<Pagination />
+								<Tbody>
+									{data.map((user) => {
+										return (
+											<>
+												<Tr key={user.id}>
+													<Td px={['4', '4', '6']}>
+														<Checkbox colorScheme="pink" />
+													</Td>
+													<Td>
+														<Box>
+															<Text fontWeight="bold">{user.name}</Text>
+															<Text fontSize="sm" color="gray.300">
+																{user.email}
+															</Text>
+														</Box>
+													</Td>
+													{isWideVersion && <Td>{user.createAt}</Td>}
+													<Td>
+														<Button
+															as="a"
+															size="sm"
+															fontSize="sm"
+															colorScheme="purple"
+															leftIcon={
+																<Icon as={RiPencilLine} fontSize="16" />
+															}
+														>
+															Editar
+														</Button>
+													</Td>
+												</Tr>
+											</>
+										)
+									})}
+								</Tbody>
+							</Table>
+							<Pagination />
+						</>
+					)}
 				</Box>
 			</Flex>
 		</Box>
